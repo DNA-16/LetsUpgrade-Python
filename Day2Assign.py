7ddedb0964de07f9bbb0716a617aaec31046548a
DarshanNA16 <68494565+DarshanNA16@users.noreply.github.com>
2020-07-23 01:23:51 +0530

    Add files via upload

---
+++ Day2Assign.py
@@ -0,0 +1,40 @@
+"""Escape Sequences
+
+\t
+\n
+
+"""
+
+#Formatted output
+name="ABCD"
+age=30
+marks=95.12345
+print("The name of person is %s and his marks is %f and his age is %d"%(name,marks,age))
+
+""" 
+Arithmetic Operators
+* Multiply
+** Exponent
+//Floor divison
+% Modulus-Reminder
+/ divison
+
+Assignment Operators
+a+=b is same as a=a+b
+a*=b is same as a=a*b
+a/=b is same as a=a/b
+
+Bitwise Operators
+| OR
+& AND
+! NOT
+
+Membership Operators
+in
+not in
+
+Identity Operators
+is not
+
+"""
+
